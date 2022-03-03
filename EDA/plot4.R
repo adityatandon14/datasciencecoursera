c0284a3f94e07c012faff995fbd3ddc4b6f15b7a
adityatandon14 <55592182+adityatandon14@users.noreply.github.com>
2022-03-03 09:05:07 +0530

    done

--- EDA/plot4.R
+++ EDA/plot4.R
@@ -1,17 +1,16 @@
 
-data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+df <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+df$Date <- as.Date(df$Date, format="%d/%m/%Y")
 
-data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+data <- subset(df, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(data_full)
 
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
+deetee <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(deetee)
 
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data, {
-    plot(Global_active_power~Datetime, type="l", 
-         ylab="Global Active Power (kilowatts)", xlab="")
+    plot(Global_active_power~Datetime, type="l",  ylab="Global Active Power (kilowatts)", xlab="")
     plot(Voltage~Datetime, type="l", ylab="Voltage (volt)", xlab="")
     plot(Sub_metering_1~Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
     lines(Sub_metering_2~Datetime,col='Red')
