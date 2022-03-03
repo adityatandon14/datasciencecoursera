caadf0013aefe14529216e57c39dd4aff963534e
adityatandon14 <55592182+adityatandon14@users.noreply.github.com>
2022-03-03 08:59:33 +0530

    Update plot1.R

--- EDA/plot1.R
+++ EDA/plot1.R
@@ -1,16 +1,13 @@
 
-data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
-
+df <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+df$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
+rm(df)
 
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
-hist(data$Global_active_power, main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-
+hist(data$Global_active_power, main="Global Active Power",  xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 dev.copy(png, file="plot1.png", height=400, width=400)
 dev.off()
